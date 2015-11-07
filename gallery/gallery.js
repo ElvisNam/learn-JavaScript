3eed5552ebb361f48f51591ded932ef0015d039e
ElvisNam <397294557@qq.com>
2015-11-07 19:55:01 +0800

    change the js

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -1,4 +1,52 @@
-window.onload = prepareGallery;
+function addLoadEvent(func)
+{
+	var oldonload = window.onload;
+
+	if (typeof window.onload != 'function') 
+		window.onload = func;
+	else 
+	{
+		window.onload = function()
+		{
+			oldonload();
+			func();
+		}
+	}
+}
+
+function insertAfter(newElement, targetElement)
+{
+	var parent = targetElement.parentNode;
+
+	if (parent.lastChild == targetElement)
+		parent.appendChild(newElement);
+	else 
+	{
+		parent.insertBefore(newElement, targetElement.nextSibling);
+	}
+}
+
+function preparePlaceholder()
+{
+	if(!document.createElement) return false;
+	if(!document.createTextNode) return false;
+
+	var placeholder = document.createElement('img');
+	placeholder.setAttribute('id', 'placeholder');
+	placeholder.setAttribute('src', 'images/placeholder.gif');
+	placeholder.setAttribute('lat', 'my image gallery');
+
+	var description = document.createElement('p');
+	description.setAttribute('id', 'description');
+
+	var destext = document.createTextNode('choose an image');
+	description.appendChild(destext);
+	placeholder.appendChild(description);
+
+	var gallery = document.getElementById('imagegallery');
+	insertAfter(placeholder, gallery);
+	insertAfter(description, placeholder);
+}
 
 function prepareGallery() 
 {
@@ -7,7 +55,7 @@ function prepareGallery()
 	if (!document.getElementById('imagegallery')) return false;
 
 	var gallery = document.getElementById('imagegallery');
-	var links = document.getElementsByTagName('a');
+	var links = gallery.getElementsByTagName('a');
 
 	for (var i=0; i < links.length; i++)
 	{
@@ -42,3 +90,6 @@ function showPic(whichpic)
 
 	return true;
 }
+
+addLoadEvent(preparePlaceholder);
+addLoadEvent(prepareGallery);
