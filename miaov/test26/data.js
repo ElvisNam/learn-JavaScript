eae7fa5168a51e460d1ab419d246f37a6b7809c6
ElvisNam <397294557@qq.com>
2016-03-31 22:39:27 +0800

    add new test

---
+++ miaov/test26/data.js
@@ -0,0 +1,191 @@
+//data
+
+var data = [
+	{
+		"name" : "前端", 
+		"list" : [
+			{
+				"title": "前端工程师", 
+				"company": "北京123公司",
+				"position": "前端页面", 
+				"recruitingNumbers": "2人",
+				"workingLocation": "北京", 
+				"workExperience": "不限", 
+				"education": "不限",
+				"wage": "面议",
+				"addData": "2016-4-1"
+			},
+			{
+				"title": "网站前端页面设计", 
+				"company": "北京ads公司",
+				"position": "前端页面", 
+				"recruitingNumbers": "2人",
+				"workingLocation": "北京", 
+				"workExperience": "不限", 
+				"education": "不限",
+				"wage": "面议",
+				"addData": "2016-4-1"
+			},
+			{
+				"title": "HTML5工程师", 
+				"company": "上海123公司",
+				"position": "前端页面", 
+				"recruitingNumbers": "2人",
+				"workingLocation": "上海", 
+				"workExperience": "不限", 
+				"education": "不限",
+				"wage": "面议",
+				"addData": "2016-4-1"
+			},
+			{
+				"title": "页面设计师", 
+				"company": "ad123公司",
+				"position": "前端页面", 
+				"recruitingNumbers": "2人",
+				"workingLocation": "北京", 
+				"workExperience": "不限", 
+				"education": "不限",
+				"wage": "面议",
+				"addData": "2016-4-1"
+			},
+			{
+				"title": "高级前端工程师", 
+				"company": "上海公司",
+				"position": "前端页面", 
+				"recruitingNumbers": "2人",
+				"workingLocation": "上海", 
+				"workExperience": "不限", 
+				"education": "不限",
+				"wage": "面议",
+				"addData": "2016-4-1"
+			},
+			{
+				"title": "前端开发", 
+				"company": "杭州公司",
+				"position": "前端页面", 
+				"recruitingNumbers": "2人",
+				"workingLocation": "杭州", 
+				"workExperience": "不限", 
+				"education": "不限",
+				"wage": "面议",
+				"addData": "2016-4-1"
+			},
+			{
+				"title": "武汉前端工程师", 
+				"company": "武汉公司",
+				"position": "前端页面", 
+				"recruitingNumbers": "2人",
+				"workingLocation": "武汉", 
+				"workExperience": "不限", 
+				"education": "不限",
+				"wage": "面议",
+				"addData": "2016-4-1"
+			},
+			{
+				"title": "长沙前端工程师", 
+				"company": "长沙公司",
+				"position": "前端页面", 
+				"recruitingNumbers": "2人",
+				"workingLocation": "长沙", 
+				"workExperience": "不限", 
+				"education": "不限",
+				"wage": "面议",
+				"addData": "2016-4-1"
+			},
+			{
+				"title": "重庆前端工程师", 
+				"company": "重庆公司",
+				"position": "前端页面", 
+				"recruitingNumbers": "2人",
+				"workingLocation": "重庆", 
+				"workExperience": "不限", 
+				"education": "不限",
+				"wage": "面议",
+				"addData": "2016-4-1"
+			},
+			{
+				"title": "前端开发师", 
+				"company": "重庆公司",
+				"position": "前端页面", 
+				"recruitingNumbers": "2人",
+				"workingLocation": "重庆", 
+				"workExperience": "不限", 
+				"education": "不限",
+				"wage": "面议",
+				"addData": "2016-4-1"
+			},
+			{
+				"title": "成都前端工程师", 
+				"company": "成都",
+				"position": "前端页面", 
+				"recruitingNumbers": "2人",
+				"workingLocation": "成都", 
+				"workExperience": "不限", 
+				"education": "不限",
+				"wage": "面议",
+				"addData": "2016-4-1"
+			},
+			{
+				"title": "厦门前端工程师", 
+				"company": "厦门公司",
+				"position": "前端页面", 
+				"recruitingNumbers": "2人",
+				"workingLocation": "厦门", 
+				"workExperience": "不限", 
+				"education": "不限",
+				"wage": "面议",
+				"addData": "2016-4-1"
+			}
+		]
+	},
+	{
+		"name": "后端", 
+		"list": [
+			{
+				"title": "后端工程师", 
+				"company": "北京123公司",
+				"position": "服务器", 
+				"recruitingNumbers": "2人",
+				"workingLocation": "北京", 
+				"workExperience": "不限", 
+				"education": "不限",
+				"wage": "面议",
+				"addData": "2016-4-1"
+			},
+			{	
+				"title": "高级后端工程师", 
+				"company": "上海123公司",
+				"position": "服务器", 
+				"recruitingNumbers": "2人",
+				"workingLocation": "上海", 
+				"workExperience": "不限", 
+				"education": "不限",
+				"wage": "面议",
+				"addData": "2016-4-1"
+			},
+			{
+				"title": "Python工程师", 
+				"company": "北京123公司",
+				"position": "服务器", 
+				"recruitingNumbers": "2人",
+				"workingLocation": "北京", 
+				"workExperience": "不限", 
+				"education": "不限",
+				"wage": "面议",
+				"addData": "2016-4-1"
+			},
+			{
+				"title": "PHP工程师", 
+				"company": "重庆123公司",
+				"position": "服务器", 
+				"recruitingNumbers": "2人",
+				"workingLocation": "重庆", 
+				"workExperience": "不限", 
+				"education": "不限",
+				"wage": "面议",
+				"addData": "2016-4-1"
+			}
+		]
+	}
+];
+//alert(data[0].name);
